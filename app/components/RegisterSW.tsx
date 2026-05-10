29a3567deb464e64ddae6735f2aee90ee4c575d6
cptstaf2018-cmd <cptstaf2018@gmail.com>
2026-05-10 16:07:48 +0300

    Fix local service worker registration

--- app/components/RegisterSW.tsx
+++ app/components/RegisterSW.tsx
@@ -3,7 +3,27 @@ import { useEffect } from "react";
 
 export default function RegisterSW() {
   useEffect(() => {
-    if ("serviceWorker" in navigator) {
+    if (!("serviceWorker" in navigator)) return;
+
+    if (process.env.NODE_ENV !== "production") {
+      navigator.serviceWorker
+        .getRegistrations()
+        .then((registrations) =>
+          Promise.all(registrations.map((registration) => registration.unregister()))
+        )
+        .catch(() => {});
+
+      if ("caches" in window) {
+        caches
+          .keys()
+          .then((keys) => Promise.all(keys.map((key) => caches.delete(key))))
+          .catch(() => {});
+      }
+
+      return;
+    }
+
+    if (window.location.protocol === "https:") {
       navigator.serviceWorker.register("/sw.js");
     }
   }, []);
